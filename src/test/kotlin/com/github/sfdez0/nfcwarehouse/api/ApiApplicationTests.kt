ee5e2c62b29f3419d2696f68b302a02828c07f38
Sergio Fernández <sergiofdez2000@gmail.com>
2026-04-17 21:21:29 +0200

    feat: initial commit

---
+++ src/test/kotlin/com/github/sfdez0/nfcwarehouse/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.sfdez0.nfcwarehouse.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
